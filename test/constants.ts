fd863c1300134a1cc8b9062a200c6d669ae1ce9c
Kauã Machado Grathwohl <Kaua.Grathwohl@thomsonreuters.com>
2025-06-17 08:33:59 -0300

    BugFix: Login Tests

--- test/constants.ts
+++ test/constants.ts
@@ -1,13 +1,6 @@
-export const { CUSTOM_TRANSFER_TOKEN_ACCESS_KEY } = require('./app-template/src/constants');
-
-export const TITLE_LOGIN = 'Strapi Admin';
-export const TITLE_HOME = 'Homepage | Strapi';
-
 export const ADMIN_EMAIL_ADDRESS = 'admin@satc.edu.br';
 export const ADMIN_PASSWORD = 'welcomeToStrapi123';
 
 export const INVALID_EMAIL_ADDRESS = 'invalid-email@satc.edu.br';
 export const INVALID_ADMIN_PASSWORD = 'welcome42222333';
 
-export const EDITOR_EMAIL_ADDRESS = 'editor@satc.edu.br';
-export const EDITOR_PASSWORD = 'welcomeToStrapi123';
